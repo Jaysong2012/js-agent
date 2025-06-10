e0f8f57d7206055c8cf67f4d2a97e24e2ee5470a
songchao09 <songchao09@meituan.com>
2025-06-10 10:13:52 +0800

    first commit

---
+++ src/main/java/cn/apmen/jsagent/JsAgentApplication.java
@@ -0,0 +1,13 @@
+package cn.apmen.jsagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/apmen/jsagent/JsAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.apmen.jsagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
